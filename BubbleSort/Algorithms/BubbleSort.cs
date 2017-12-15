5591275d8ae7ccede79a0d9dedf1b9ff1814a18b
Asfand Tareen <atareen@adps.com>
2017-12-14 16:20:14 -0800

    Basic structure for integers.

---
+++ BubbleSort/Algorithms/BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSort.Algorithms
+{
+    class BubbleSort
+    {
+        public int[] List { get; set; }
+        public bool OrderBy { get; set; }
+
+        public BubbleSort()
+        {
+            this.List = new int[]{};
+            this.OrderBy = true;
+        }
+
+        public int[] SortIntAsc(int[] list)
+        {
+            return new int[]{};
+        }
+
+        public int[] SortIntDesc(int[] list)
+        {
+            return new int[] {};
+        }
+
+    }
+}
